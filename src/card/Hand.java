8028133d31c82699fd22ce9fc836c277b4193ddb
Tom <tomfraisse.pro@gmail.com>
2024-04-24 13:45:22 +0200

    Setup Hand / GameController classes

--- src/card/Hand.java
+++ src/card/Hand.java
@@ -2,6 +2,25 @@ package card;
 
 
 public class Hand {
-	private Card[] hand;
+	private Card[] currentHand;
+	
+	public Hand() {
+		this.currentHand = new Card[4];
+	}
+	public void add(Card e) {
+		
+	}
+	public void remove(Card e) {
+		
+	}
+	public void discardAllHand() {
+		
+	}
+	public void discardMiddleHand() {
+		
+	}
+	public void hideLastCard() {
+		
+	}
 	
 }
--- src/game/GameController.java
+++ src/game/GameController.java
@@ -1,5 +1,43 @@
 package game;
 
 public class GameController {
-
+	private int remainingJoker;
+	private int pointScored;
+	
+	public GameController() {
+		this.remainingJoker = 3;
+		this.pointScored = 0;
+	}
+	public int getLastJoker() {
+		return remainingJoker;
+	}
+	public int getScore() {
+		return pointScored;
+	}
+	
+	public void start() {
+		
+	}
+	public void stop() {
+		
+	}
+	
+	public void scoreFivePoints() {
+		// discard all the hand 
+		
+		// add points 
+		this.pointScored += 5;
+	}
+	public void scoreTwoPoints() {
+		// discard middle of  the hand 
+		
+		// add points 
+		this.pointScored += 2;
+	}
+	
+	public void useJoker() {
+		// hide last Card
+		
+		// get another Card at the top of the Hand
+	}
 }
